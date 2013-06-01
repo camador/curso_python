b7da551b7b2ccc41e39ea29863bcb3b2ecfab15c
camador <camador.git@gmail.com>
2013-06-01 18:59:55 +0200

    Extrayendo la información usando selectores

--- tema5/posts_osl/posts_osl/spiders/osl_spider.py
+++ tema5/posts_osl/posts_osl/spiders/osl_spider.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
 
 class oslSpider(BaseSpider):
 
@@ -7,6 +10,30 @@ class oslSpider(BaseSpider):
     start_urls = ['http://osl.ugr.es/']
 
     def parse(self, response):
-        pass
-    
 
+        #
+        # Selectores:
+        #
+        # Posts: "//div[@class='entry hentry']"
+        # Título:  "//div[@class='entry hentry']/h2[@class='entry-title']/a/text()'"
+        # Autor: "//div[@class='entry hentry']/div[@class='entry-byline']/address[@class='author vcard']/a/text()"
+        # Contenido: "//div[@class='entry hentry']/div[@class='entry-content']"
+        # Categorías: "//div[@class='entry hentry']/p[@class='entry-meta']/span[@class='entry-categories']/a/text()"
+        # Etiquetas: "//div[@class='entry hentry']/p[@class='entry-meta']/span[@class='entry-tags']/a/text()"
+
+        # Instancia un selector
+        hxs = HtmlXPathSelector(response)
+
+        # Selecciona los posts de la página principal
+        posts = hxs.select("//div[@class='entry hentry']")
+
+        # Recorre los posts extrayendo la información
+        for post in posts:
+            print '--------------------'
+            print u'Título: ', post.select("h2[@class='entry-title']/a/text()").extract()[0]
+            print u'Autor: ', post.select("div[@class='entry-byline']/address[@class='author vcard']/a/text()").extract()[0]
+            print u'Contenido: ', post.select("div[@class='entry-content']").extract()[0]
+            print u'Categorías: ', ', '.join(post.select("p[@class='entry-meta']/span[@class='entry-categories']/a/text()").extract())
+            print u'Tags: ', ', '.join(post.select("p[@class='entry-meta']/span[@class='entry-tags']/a/text()").extract())
+
+        print '--------------------'
