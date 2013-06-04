d49d11678ebb5f12667a347d64cdbd82560b5fea
camador <camador.git@gmail.com>
2013-06-04 21:59:56 +0200

    - Implementando el movimiento del personaje (por ahora sólo hacia   arriba) - Establecido el framerate mediante una constante

--- tema4/gemas/gemas.py
+++ tema4/gemas/gemas.py
@@ -36,6 +36,9 @@ IMG_DIR = 'imagenes'
 # Velocidad base para todos los sprites
 VELOCIDAD_BASE = 1
 
+# Número de frames por segundo
+FRAMERATE = 60
+
 ##
 ## JUGADOR
 ##
@@ -63,6 +66,27 @@ class Jugador(pygame.sprite.Sprite):
         # Velocidad de movimiento
         self.velocidad = VELOCIDAD_BASE
 
+    def mover(self, tiempo):
+        """
+            Gestiona el movimiento del personaje
+
+            El cálculo de la posición del personaje se realiza en función de la velocidad y
+            del tiempo (d = v * t, distancia = velocidad * tiempo), o sea, la nueva posición
+            será igual a la posición actual más la distancia recorrida en el eje correspondiente
+        """
+
+        # Los límites del movimiento son los bordes de la ventana
+        if self.rect.top >= 0 and self.rect.bottom <= ALTO and self.rect.left >= 0 and self.rect.right <= ANCHO:
+
+            # Obtiene las pulsaciones de teclas
+            teclas = pygame.key.get_pressed()
+
+            # Cursor Arriba
+            if teclas[K_UP]:
+
+                # Desplazamiento hacia arriba
+                self.rect.centery -=  self.velocidad * tiempo
+
 ##
 ## MAIN
 ##
@@ -83,8 +107,16 @@ def main():
         # Instancia al jugador
         jugador = Jugador()
 
+        # Instancia un reloj para controlar el tiempo
+        reloj = pygame.time.Clock()
+
         # El programa permanece funcionando hasta que se cierra la ventana
         while True:
+
+            # Averigua el tiempo (en milisegundos) transcurrido por cada frame (cada iteración del bucle)
+            # Además, al usar FRAMERATE en la llamada, se fija el número de frames por segundo 
+            # independientemente del hardware de la máquina
+            tiempo = reloj.tick(FRAMERATE)
 
             # Obtiene y recorre la lista de eventos que están teniendo lugar
             for evento in pygame.event.get():
@@ -93,11 +125,24 @@ def main():
                 if evento.type == QUIT:
                     sys.exit(0)
 
+            #
+            # CALCULO DEL MOVIMIENTO
+            # 
+            jugador.mover(tiempo)
+
+            #
+            # ACTUALIZACIÓN DE POSICIONES EN PANTALLA
+            # 
+
             # Situa el fondo en el primer pixel de la ventana
             ventana.blit(fondo, (0, 0))
 
             # Situa al jugador en la ventana
             ventana.blit(jugador.imagen, jugador.rect)
+
+            #
+            # ACTUALIZACIÓN DE LA PANTALLA
+            # 
 
             # Dibuja la escena
             pygame.display.flip()
