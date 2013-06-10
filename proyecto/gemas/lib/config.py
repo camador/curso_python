190a8c215579a199fde6d79131e8ecae32eee5a4
camador <camador.git@gmail.com>
2013-06-10 11:22:39 +0200

    Añadida inicialización de los valores de configuración

--- proyecto/gemas/lib/config.py
+++ proyecto/gemas/lib/config.py
@@ -5,8 +5,46 @@ class Config():
     """
         Valores de configuración del juego
     """
-    def __init__(self):
-        pass
+
+    def __init__(self, db):
+        """
+            Establece los valores de configuración
+        """
+
+        # 
+        # Toma los valores de la base de datos
+        # 
+
+        # Tamaño de la ventana
+        self.ventana_ancho = int(db.get_config('VENTANA_ANCHO'))
+        self.ventana_alto = int(db.get_config('VENTANA_ALTO'))
+
+        # Ubicación de los ficheros
+        self.dir_img = db.get_config('DIR_IMG')
+
+        # Velocidad base para todos los sprites
+        self.velocidad_base = int(db.get_config('VELOCIDAD_BASE'))
+
+        # Número de frames por segundo
+        self.framerate = int(db.get_config('FRAMERATE'))
+ 
+        # 
+        # Otros valores no tomados de la base de datos
+        # 
+
+        # Nombre de los ejes
+        self.eje_x = 'x'
+        self.eje_y = 'y'
+
+        # Puntos de spawn (las 4 esquinas)
+        self.spawn_margen_x = self.ventana_ancho / 10
+        self.spawn_margen_y = self.ventana_alto / 8
+        self.spawn_points = [
+                                (self.spawn_margen_x, self.spawn_margen_y),
+                                (self.spawn_margen_x, self.ventana_alto - self.spawn_margen_y),
+                                (self.ventana_ancho - self.spawn_margen_x, self.spawn_margen_y),
+                                (self.ventana_ancho - self.spawn_margen_x, self.ventana_alto - self.spawn_margen_y)
+                            ]
 
 if __name__ == '__main__':
     print u'Módulo no ejecutable.'
