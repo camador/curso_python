1f06fcd589ff86c8726be550eb3c7db32dba9e49
camador <camador.git@gmail.com>
2013-06-10 19:49:08 +0200

    Añadido fichero que contendrá la clase encargada de gestionar la herramienta de configuración

---
+++ proyecto/gemas/lib/gui.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class GUI():
+    """
+        Herramienta de configuración
+    """
+
+    def __init__(self):
+        pass
+    
+if __name__ == '__main__':
+    print u'Módulo no ejecutable.'
+
