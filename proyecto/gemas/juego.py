d87e57e04c56c7ce61585ec16a1ff48e55bef177
camador <camador.git@gmail.com>
2013-06-10 11:23:47 +0200

    Añadida inicialización de la configuración mediante las clases gestoras de base de datos y configuración

--- proyecto/gemas/juego.py
+++ proyecto/gemas/juego.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Base de datos
+from lib.db import DB
+
+# Juego
+from lib.config import Config
+
 def main():
-    print 'Empezando...'
+
+    # Gestor de base de datos
+    db = DB()
+
+    # Carga los valores de configuración
+    config = Config(db)
+
     return 0
 
 if __name__ == '__main__':
