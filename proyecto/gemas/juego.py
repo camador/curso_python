b67717d3fe4f7cf6691de6e2326054562e1444ec
camador <camador.git@gmail.com>
2013-06-10 11:43:10 +0200

    Creación de ventana y bucle de eventos

--- proyecto/gemas/juego.py
+++ proyecto/gemas/juego.py
@@ -7,15 +7,55 @@ from lib.db import DB
 # Juego
 from lib.config import Config
 
+# Pygame
+import pygame
+from pygame.locals import *
+
+# Otros
+import os
+import sys
+
 def main():
 
     try :
+
+        #
+        # CONFIGURACIÓN
+        #
 
         # Gestor de base de datos
         db = DB()
 
         # Carga los valores de configuración
         config = Config(db)
+
+        #
+        # VENTANA
+        #
+
+        # Crea la ventana
+        ventana = pygame.display.set_mode((config.ventana_ancho, config.ventana_alto))
+
+        # Título de la ventana
+        pygame.display.set_caption('Gemas')
+
+        # Carga el fondo (convirtiéndolo al formato usado en SDL para mejorar la eficiencia)
+        fondo = pygame.image.load(os.path.join(config.dir_img, 'fondo.jpg')).convert()
+
+        #
+        # BUCLE DE EVENTOS
+        #
+
+        # El programa permanece funcionando hasta que se cierra la ventana
+        # Cada iteración del bucle es un frame
+        while True:
+
+            # Obtiene y recorre la lista de eventos que están teniendo lugar
+            for evento in pygame.event.get():
+
+                # Si encuentra el evento QUIT termina la ejecución
+                if evento.type == QUIT:
+                    sys.exit(0)
 
         return 0
 
